e0f9c9b8c20ad4ad126177960bd4a17d7dca16d4
andreseichi <andre_seichi@hotmail.com>
2022-12-26 16:07:15 -0300

    rota página address

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { CreateUserFormLayout } from "./layouts/CreateUserFormLayout";
 import { UserInfo } from "./pages/Form/UserInfo";
+import { UserAddress } from "./pages/Form/UserAddress";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<CreateUserFormLayout />}>
         <Route path="/" element={<UserInfo />} />
-        {/* <Route path="address" element={<Home />} />
-        <Route path="about" element={<History />} />
-        <Route path="success" element={<History />} /> */}
+        <Route path="address" element={<UserAddress />} />
       </Route>
     </Routes>
   );
