b1c0490805642510d3801f5a64ba5ee58731d2dd
andreseichi <andre_seichi@hotmail.com>
2022-12-26 17:50:53 -0300

    providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,20 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App";
+import { BrowserRouter } from "react-router-dom";
 import { UserFormProvider } from "./context/UserFormContext";
+import { Router } from "./Router";
 
 import { theme } from "./styles/theme";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <UserFormProvider>
-        <App />
-      </UserFormProvider>
+      <BrowserRouter>
+        <UserFormProvider>
+          <Router />
+        </UserFormProvider>
+      </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
 );
